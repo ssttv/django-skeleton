21c9ff15ea2a3e373a593816a8de7defd96fd670
Sergey Titov <sstmail@mail.ru>
2019-02-03 20:46:09 +0300

    feat: created first model

--- leadmanager/leads/models.py
+++ leadmanager/leads/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Lead(models.Model):
+    name = models.models.CharField(max_length=100)
+    email = models.models.EmailField(max_length=100, unique=True)
+    message = models.models.CharField(max_length=500, blank=True)
+    created_at = models.models.DateTimeField(auto_now_add=True)
+    
