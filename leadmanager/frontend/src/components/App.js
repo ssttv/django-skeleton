f963324631a8a93057c784506d0a1d062266da79
Sergey Titov <sstmail@mail.ru>
2019-02-06 11:28:00 +0300

    feat: build header

--- leadmanager/frontend/src/components/App.js
+++ leadmanager/frontend/src/components/App.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 
+import Header from './layout/Header'
+
 class App extends Component {
     render() {
-        return <h1>Here be some React code</h1>
+        return (
+            <Header />
+        )
     } 
 }
 
