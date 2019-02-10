c90270c39e55a60d7605af42a2f4e25e4c873af0
Sergey Titov <sstmail@mail.ru>
2019-02-10 21:10:59 +0300

    feat: implement lead addition

--- leadmanager/frontend/src/actions/leads.js
+++ leadmanager/frontend/src/actions/leads.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-import { GET_LEADS, DELETE_LEAD } from "./types";
+import { GET_LEADS, DELETE_LEAD, ADD_LEAD } from "./types";
 
 //GET_LEADS
 export const getLeads = () => dispatch => {
@@ -16,6 +16,19 @@ export const getLeads = () => dispatch => {
 };
 
 //DELETE_LEAD
+export const addLead = lead => dispatch => {
+  axios
+    .post(`/api/leads/`, lead)
+    .then(res => {
+      dispatch({
+        type: ADD_LEAD,
+        payload: res.data
+      });
+    })
+    .catch(err => console.log(err));
+};
+
+//ADD_LEAD
 export const deleteLead = id => dispatch => {
   axios
     .delete(`/api/leads/${id}/`)
@@ -27,5 +40,3 @@ export const deleteLead = id => dispatch => {
     })
     .catch(err => console.log(err));
 };
-
-//ADD_LEAD
