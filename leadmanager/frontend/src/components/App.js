94fb10c482f6a67aa47fb9974549a2b76f2e4182
Sergey Titov <sstmail@mail.ru>
2019-02-10 23:03:37 +0300

    feat: add alert provider

--- leadmanager/frontend/src/components/App.js
+++ leadmanager/frontend/src/components/App.js
@@ -3,19 +3,29 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import store from "../store";
 
+import { Provider as AlertProvider } from "react-alert";
+import AlertTemplate from "react-alert-template-basic";
+
 import Header from "./layout/Header";
 import Dashboard from "./leads/Dashboard";
+
+const alertOptions = {
+  timeout: 3000,
+  position: "top center"
+};
 
 class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <Fragment>
-          <Header />
-          <div className="container">
-            <Dashboard />
-          </div>
-        </Fragment>
+        <AlertProvider template={AlertTemplate} {...alertOptions}>
+          <Fragment>
+            <Header />
+            <div className="container">
+              <Dashboard />
+            </div>
+          </Fragment>
+        </AlertProvider>
       </Provider>
     );
   }
